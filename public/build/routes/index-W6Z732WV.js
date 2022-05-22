f931078f1cdaa0971c77acdeab4339308f806c51
= <=>
2022-05-22 15:46:27 -0400

    paula #2 swapped in Remix tech stack

---
+++ public/build/routes/index-W6Z732WV.js
@@ -0,0 +1,65 @@
+import {
+  useOptionalUser
+} from "/build/_shared/chunk-SCAE34XM.js";
+import {
+  Link
+} from "/build/_shared/chunk-SKDCIZ45.js";
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-6BO74FWO.js";
+
+// browser-route-module:/Users/paula/code/MusicTree/trap-stack/app/routes/index.tsx?browser
+init_react();
+
+// app/routes/index.tsx
+init_react();
+function Index() {
+  const user = useOptionalUser();
+  return /* @__PURE__ */ React.createElement("main", {
+    className: "relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "relative sm:pb-16 sm:pt-8"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "mx-auto max-w-7xl sm:px-6 lg:px-8"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "relative shadow-xl sm:overflow-hidden sm:rounded-2xl"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "absolute inset-0"
+  }, /* @__PURE__ */ React.createElement("img", {
+    className: "h-full w-full object-cover",
+    src: "https://raw.githubusercontent.com/PaulaPaul/assets/main/TempSplashScreen.jpg?raw=true",
+    alt: "temporary landing page graphic - https://www.flickr.com/photos/12508224@N02/5426124124 Creative Commons License "
+  }), /* @__PURE__ */ React.createElement("div", {
+    className: "absolute inset-0 bg-[color:rgba(139,92,246,0.5)] mix-blend-multiply"
+  })), /* @__PURE__ */ React.createElement("div", {
+    className: "lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32"
+  }, /* @__PURE__ */ React.createElement("h1", {
+    className: "text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl"
+  }, /* @__PURE__ */ React.createElement("span", {
+    className: "block uppercase text-violet-500 drop-shadow-md"
+  }, "MusicTree.Me")), /* @__PURE__ */ React.createElement("p", {
+    className: "mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl"
+  }, "Connect to musicians around the world and explore your musical heritage."), /* @__PURE__ */ React.createElement("div", {
+    className: "mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center"
+  }, user ? /* @__PURE__ */ React.createElement(Link, {
+    to: "/notes",
+    className: "flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-violet-700 shadow-sm hover:bg-violet-50 sm:px-8"
+  }, "View Notes for ", user.email) : /* @__PURE__ */ React.createElement("div", {
+    className: "space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0"
+  }, /* @__PURE__ */ React.createElement(Link, {
+    to: "/join",
+    className: "flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-violet-700 shadow-sm hover:bg-violet-50 sm:px-8"
+  }, "Sign up"), /* @__PURE__ */ React.createElement(Link, {
+    to: "/login",
+    className: "flex items-center justify-center rounded-md bg-violet-500 px-4 py-3 font-medium text-white hover:bg-violet-600  "
+  }, "Log In")))))), /* @__PURE__ */ React.createElement("div", {
+    className: "mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8"
+  }, /* @__PURE__ */ React.createElement("div", {
+    className: "mt-6 flex flex-wrap justify-center gap-8"
+  }))));
+}
+export {
+  Index as default
+};
+//# sourceMappingURL=/build/routes/index-W6Z732WV.js.map
---
+++ public/build/routes/notes/new-G5QCUM5S.js
@@ -0,0 +1,53 @@
+import {
+  require_note
+} from "/build/_shared/chunk-2YJZUK4K.js";
+import {
+  require_session
+} from "/build/_shared/chunk-ME5PAYV3.js";
+import {
+  Form
+} from "/build/_shared/chunk-COFHBGIN.js";
+import {
+  React,
+  __toESM,
+  init_react
+} from "/build/_shared/chunk-6BO74FWO.js";
+
+// browser-route-module:/Users/paula/code/MusicTree/trap-stack/app/routes/notes/new.tsx?browser
+init_react();
+
+// app/routes/notes/new.tsx
+init_react();
+var import_note = __toESM(require_note());
+var import_session = __toESM(require_session());
+function NewNotePage() {
+  return /* @__PURE__ */ React.createElement(Form, {
+    method: "post",
+    style: {
+      display: "flex",
+      flexDirection: "column",
+      gap: 8,
+      width: "100%"
+    }
+  }, /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("label", {
+    className: "flex w-full flex-col gap-1"
+  }, /* @__PURE__ */ React.createElement("span", null, "Title: "), /* @__PURE__ */ React.createElement("input", {
+    name: "title",
+    className: "flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+  }))), /* @__PURE__ */ React.createElement("div", null, /* @__PURE__ */ React.createElement("label", {
+    className: "flex w-full flex-col gap-1"
+  }, /* @__PURE__ */ React.createElement("span", null, "Body: "), /* @__PURE__ */ React.createElement("textarea", {
+    name: "body",
+    rows: 8,
+    className: "w-full flex-1 rounded-md border-2 border-blue-500 py-2 px-3 text-lg leading-6"
+  }))), /* @__PURE__ */ React.createElement("div", {
+    className: "text-right"
+  }, /* @__PURE__ */ React.createElement("button", {
+    type: "submit",
+    className: "rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+  }, "Save")));
+}
+export {
+  NewNotePage as default
+};
+//# sourceMappingURL=/build/routes/notes/new-G5QCUM5S.js.map
