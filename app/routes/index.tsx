bc707ddc803afa9cc403e546c72e625d6bf08edf
Erica Edge <hello@duggie.dev>
2022-05-24 14:44:00 -0400

    adds redirect to explore pg when logged in, home test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,29 +1,23 @@
 import { Link } from "@remix-run/react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useOptionalUser } from "~/utils";
 
 export default function Index() {
   const user = useOptionalUser();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      navigate("/explore");
+    }
+  }, [user, navigate]);
+
   return (
     <main>
-      Landing Page
-      {!user ? (
-        <Link to="/notes">View Notes for foo</Link>
-      ) : (
-        <div>
-          <Link
-            to="/join"
-            className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-violet-700 shadow-sm hover:bg-violet-50 sm:px-8"
-          >
-            Sign up
-          </Link>
-          <Link
-            to="/login"
-            className="flex items-center justify-center rounded-md bg-violet-500 px-4 py-3 font-medium text-white hover:bg-violet-600  "
-          >
-            Log In
-          </Link>
-        </div>
-      )}
+      <div>
+        <Link to="/join">Sign up</Link>
+        <Link to="/login">Log In</Link>
+      </div>
     </main>
   );
 }
