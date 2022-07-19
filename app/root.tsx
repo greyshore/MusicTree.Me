a114e1d88cda9311459a214687fa7f968ba42019
Erica Edge <hello@duggie.dev>
2022-07-18 21:20:38 -0400

    styles on about page

--- app/root.tsx
+++ app/root.tsx
@@ -27,7 +27,7 @@ export let links: LinksFunction = () => {
     { rel: "preconnect", href: "https://fonts.gstaticom" },
     {
       rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Sofia&display=swap",
     },
     {
       rel: "stylesheet",
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -11,26 +11,38 @@ export default function About() {
 
   return (
     <Container as="main" maxW="6xl">
-      <VStack spacing={8}>
-        <Center>
-          <Text>
-          <Heading>A musical family tree and resource</Heading>
-
-          At MusicTree.me, we provide an opportunity for musicians to explore their connections to the generations of musicians from which they are pedagogically descended.
-          Discover intersecting paths and common roots to understand your musical heritage. Explore the landscape of constellation teachers and their impact on the global music community.
-          <Text>
-          Check back frequently – your musical family tree is shaped by data shared by other users, so it will grow and change as more users share information.
-          </Text>
-          This is a project for our beloved musical community. We are committed to providing accurate information and your privacy. We will never sell/share your personal information. 
-          <Text>
-          <Link to="https://www.sashabassoon.com/">Dr. Sasha Enegren</Link>
-          </Text>
-          <Text>
-          For the digitally inclined, <Link to="https://github.com/greyshore/MusicTree.Me/">contribute our project on Github!</Link>
-          </Text>
-          </Text>
-        </Center>
+      <VStack spacing={8} alignItems="flex-start">
+        <Heading>A musical family tree and resource</Heading>
+        <Text>
+          At MusicTree.me, we provide an opportunity for musicians to explore
+          their connections to the generations of musicians from which they are
+          pedagogically descended. Discover intersecting paths and common roots
+          to understand your musical heritage. Explore the landscape of
+          constellation teachers and their impact on the global music community.
+        </Text>
+        <Text>
+          Check back frequently – your musical family tree is shaped by data
+          shared by other users, so it will grow and change as more users share
+          information.
+        </Text>
+        This is a project for our beloved musical community. We are committed to
+        providing accurate information and your privacy. We will never
+        sell/share your personal information.
+        <Text sx={{ fontFamily: "'Sofia', cursive", fontSize: 26 }}>
+          <Link style={{ color: "#2B6CB0" }} to="https://www.sashabassoon.com/">
+            Dr. Sasha Enegren
+          </Link>
+        </Text>
+        <Text>
+          For the digitally inclined, contribute our project on{" "}
+          <Link
+            style={{ color: "#2B6CB0" }}
+            to="https://github.com/greyshore/MusicTree.Me/"
+          >
+            Github!
+          </Link>
+        </Text>
       </VStack>
     </Container>
-  )
+  );
 }
