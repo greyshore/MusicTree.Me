12814f8b38fc82a1c9b261868770b29d645d4831
Erica Edge <hello@duggie.dev>
2022-07-19 23:05:03 -0400

    UI for login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -9,6 +9,17 @@ import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import { verifyLogin } from "~/models/user.server";
 import { createUserSession, getUserId } from "~/session.server";
 import { validateEmail } from "~/utils";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Heading,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Input from "~/components/common/form/input";
 
 export const meta: MetaFunction = () => {
   return {
@@ -73,7 +84,6 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function Login() {
   const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get("redirectTo") ?? "/explore";
 
   const actionData = useActionData() as ActionData;
   const emailRef = React.useRef<HTMLInputElement>(null);
@@ -90,86 +100,52 @@ export default function Login() {
   }, [actionData]);
 
   return (
-    <div className="flex min-h-full flex-col justify-center">
-      <div className="mx-auto w-full max-w-md px-8">
-        <Form method="post" className="space-y-6" noValidate>
-          <div>
-            <label className="text-sm font-medium" htmlFor="email">
-              <span className="block text-gray-700">Email Address</span>
-              {actionData?.errors?.email && (
-                <span className="block pt-1 text-red-700" id="email-error">
-                  {actionData?.errors?.email}
-                </span>
-              )}
-            </label>
-            <input
-              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-              autoComplete="email"
-              type="email"
+    <Container as="main" maxW="6xl">
+      <SimpleGrid>
+        <Box>
+          <Heading as="h1" fontSize="5xl">
+            Welcome Back
+          </Heading>
+          <Text color="grey" as="span">
+            Please enter your details
+          </Text>
+        </Box>
+        <Box as={Form} method="post" noValidate>
+          <VStack spacing={8}>
+            <Input
               name="email"
-              id="email"
-              aria-invalid={actionData?.errors?.email ? true : undefined}
-              aria-describedby="email-error"
-              ref={emailRef}
+              label="Email"
+              type="email"
+              helperText="We'll never share your email."
             />
-          </div>
-          <div>
-            <label className="text-sm font-medium" htmlFor="password">
-              <span className="block text-gray-700">Password</span>
-              <span className="block font-light text-gray-700">
-                Must have at least 6 characters.
-              </span>
-              {actionData?.errors?.password && (
-                <span className="pt-1 text-red-700" id="password-error">
-                  {actionData?.errors?.password}
-                </span>
-              )}
-            </label>
-            <input
-              id="password"
-              type="password"
+            <Input
               name="password"
-              autoComplete=""
-              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-              aria-invalid={actionData?.errors?.password ? true : undefined}
-              aria-describedby="password-error"
-              ref={passwordRef}
+              label="Password"
+              type="password"
+              helperText="Must be at least 6 characters long."
             />
-          </div>
-          <button
-            className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-            type="submit"
-          >
-            Log in
-          </button>
-          <input type="hidden" name="redirectTo" value={redirectTo} />
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-                id="remember"
-                name="remember"
-                type="checkbox"
-              />
-              <label
-                className="ml-2 block text-sm text-gray-900"
-                htmlFor="remember"
+            <Box w="100%">
+              <Button
+                type="submit"
+                colorScheme="green"
+                borderRadius="full"
+                width="100%"
+                my={10}
               >
-                Remember me
-              </label>
-            </div>
-            <div className="text-center text-sm text-gray-500">
+                Sign in
+              </Button>
+            </Box>
+          </VStack>
+          <Center>
+            <Text>
               Don't have an account?{" "}
-              <Link
-                className="text-blue-500 underline"
-                to={{ pathname: "/join" }}
-              >
-                Sign up
+              <Link to="/join" style={{ color: "#2B6CB0" }}>
+                Sign up for free.
               </Link>
-            </div>
-          </div>
-        </Form>
-      </div>
-    </div>
+            </Text>
+          </Center>
+        </Box>
+      </SimpleGrid>
+    </Container>
   );
 }
